70b49c058a8f252e2366d68bc418d167652e5917
Renan Jochem <renanjochem98@gmail.com>
2018-06-06 10:23:08 -0300

    Mudanças que fiz ontem

--- gomoku.c
+++ gomoku.c
@@ -1,6 +1,7 @@
-# include <stdio.h>
+#include <pthread.h>
 #include <time.h>
 #include <math.h>
+# include <stdio.h>
 // funcao para imprimir a matriz na tela. Apenas para despoluir a funcao principal
 void escreveMatriz(int tamMatriz, int matriz[tamMatriz][tamMatriz]){
 	int i;
@@ -24,15 +25,15 @@ int verificaVencedorHorizontal(int tamMatriz, int matriz[tamMatriz][tamMatriz], 
 	int count = 0;
 	int numPecas = tamMatriz / 2; //para nao precisar passar mais um argumento como parametro sempre
 	int vencedor = 0;
-	
+
 	for(i = 0; i < tamMatriz; i++){
 		for(j = 0; j < tamMatriz; j++){
 			if(matriz[i][j] == jogadorVez){
 				count++;
 			}else{
 				count = 0;
-			}	
-			
+			}
+
 			if(count >= numPecas){
 				break;
 			}
@@ -42,11 +43,48 @@ int verificaVencedorHorizontal(int tamMatriz, int matriz[tamMatriz][tamMatriz], 
 		}
 		count = 0;
 	}
-	
+
 	if(count >= numPecas){
 		vencedor = 1;
 	}
 	return vencedor;
+}
+int verificaVencedorVertical(int tamMatriz, int matriz[tamMatriz][tamMatriz], int jogadorVez){
+    int i;
+    int j;
+    int count = 0;
+    int numPecas = tamMatriz / 2;
+    int vencedor = 0;
+
+	for(i = 0; i < tamMatriz; i++){
+	    for(j = 0; j < tamMatriz; j++){
+			if(matriz[j][i] == jogadorVez){
+				count++;
+			}else{
+				count = 0;
+			}
+
+			if(count >= numPecas){
+				break;
+			}
+	    }
+		if(count >= numPecas){
+			break;
+		}
+		count = 0;
+	}
+
+	if(count >= numPecas){
+		vencedor = 1;
+	}
+	return vencedor;
+}
+int verificaEmpate(int contTotalPec, int tamMatriz, int matriz[tamMatriz][tamMatriz], int jogadorVez){
+    int verificaEmpate;
+    if(contTotalPec == 0){
+        return verificaEmpate = 1;
+    }
+    return verificaEmpate = 0;
 }
 
 int verificaVencDiagEsqInfParaDirSup(int tamMatriz, int matriz[tamMatriz][tamMatriz], int jogadorVez){
@@ -55,132 +93,151 @@ int verificaVencDiagEsqInfParaDirSup(int tamMatriz, int matriz[tamMatriz][tamMat
 	int count = 0;
 	int numPecas = tamMatriz / 2; //para nao precisar passar mais um argumento como parametro sempre
 	int vencedor = 0;
-	
+
 	int primeiraLinha  = numPecas - 1;
 	int ultimaColuna = tamMatriz - (numPecas - 1);
-	
-	int contou = 0;
+
+
 	int coluna = 0;
-	i = primeiraLinha;
-	j = 0;
-		
-	//for(i = primeiraLinha; i < tamMatriz; i++){
-	while(i < tamMatriz && j < tamMatriz){
+
+	for(i = primeiraLinha; i < tamMatriz; i++){
 		int linha = i;
-		coluna = j;
-		if(i == tamMatriz - 1){
-			j++;
-		}
-		while(linha >= 0 || coluna <= linha){
-			printf("Linha: %d, Coluna: %d \n", linha, coluna);
-			if(matriz[linha][coluna] == jogadorVez){
-				count++;
-			}else{
-				count = 0;
-			}	
-			
-			if(count >= numPecas){
-				break;
-			}
-			//confere se peca eh a certa
+		while(linha > 0 && coluna <= linha){
+			int posicao = matriz[linha][coluna];
+
+			//comparo
 			linha--;
 			coluna++;
 		}
-		if(count >= numPecas){
-			break;
-		}
-		i++;
 	}
-	
-	//printf("Contou: %d\n", contou);
-	
+
+
 	if(count >= numPecas){
 		vencedor = 1;
 	}
 	return vencedor;
 }
 
+int verificaVencedor(int tamMatriz, int matriz[tamMatriz][tamMatriz], int jogadorVez){
+	pthread_t horizontal;
+	pthread_t vertical;
+
+	struct params {
+		int tamMatriz;
+		int matriz[tamMatriz][tamMatriz];
+		int jogadorVez;
+	};
+
+	struct params p;
+	p.tamMatriz = tamMatriz;
+
+	int vencedores[2] = {0, 0};
+
+	pthread_create(&horizontal, NULL, verificaVencedorHorizontal, &p);
+	pthread_create(&vertical, NULL, verificaVencedorVertical, &p);
+
+	pthread_join(&horizontal, vencedores[0]);
+	pthread_join(&vertical, vencedores[1]);
+
+	int i;
+	int maior = 0;
+	for(i = 0; i < 2; i++){
+		printf("Vencedor, retorno: %d \n", vencedores[i]);
+		if(vencedores[i] > maior){
+			maior = vencedores[i];
+		}
+	}
+
+	return maior;
+}
+
 int main(){
+	int verificaEmpate = 0;
 	int vencedor = 0;
 	int linha;
 	int coluna;
 	int tamanhoMatriz;
-	
+
 	int jogador1 = 0;
 	int jogador2 = 1;
-	
+
 	int i;
 	int j;
-	
+
 	printf("Digite o numero de pedras: \n");
 	scanf("%d", &tamanhoMatriz);
-	
+
 	int jogadorVez = (rand() % 2);
-	
+
 	// a matriz tem que ter o dobro de numero de pedras
 	tamanhoMatriz *= 2;
-	
+
 	int matriz[tamanhoMatriz][tamanhoMatriz];
-	
+
+	int contaPecaTotal = tamanhoMatriz * tamanhoMatriz;
+
 	// inicializa a matriz
 	for(i = 0; i < tamanhoMatriz; i++){
 			for(j = 0; j < tamanhoMatriz; j++){
 				matriz[i][j] = 0;
 			}
 		}
-	
+
 	// enquanto nao houver vencedor... ao menos ate implementarmos isso
 	while(vencedor < 1){
-		
+
 		// imprime a matriz na tela
 		escreveMatriz(tamanhoMatriz, matriz);
-		
+
 		// paga entrada de linha e coluna do teclado
 		printf("Vez do jogador %d \n", jogadorVez + 1);
 		printf("Digite a linha e a coluna: \n");
 		scanf("%d %d", &linha, &coluna);
-		
+
 		// enquanto usuario nao digitar uma posicao valida, o algoritmo fica preso aqui, mostrando o erro e pedindo nova posicao
-		while((linha >= tamanhoMatriz || coluna >= tamanhoMatriz) || matriz[linha][coluna] != 0){
+	    while((linha >= tamanhoMatriz || coluna >= tamanhoMatriz) || matriz[linha][coluna] != 0){
 			if(linha >= tamanhoMatriz){
 				printf("Voce selecionou uma linha invalida. \n");
 			}
-			
+
 			if(coluna >= tamanhoMatriz){
 				printf("Voce selecionou uma coluna invalida. \n");
 			}
-			
+
 			if(matriz[linha][coluna] != 0){
 				printf("Voce selecionou uma posicao ocupada. \n");
 			}
-			
+
 			printf("Digite a linha e a coluna, validas: \n");
 			scanf("%d %d", &linha, &coluna);
 		}
-		
 		// imprime o valor referente ao jogador
 		matriz[linha][coluna] = jogadorVez + 1;
-		
-		
-		//vencedor = verificaVencedorHorizontal(tamanhoMatriz, matriz, jogadorVez + 1);
-		vencedor = verificaVencDiagEsqInfParaDirSup(tamanhoMatriz, matriz, jogadorVez + 1);
-		
+
+
+		// vencedor = verificaVencedorHorizontal(tamanhoMatriz, matriz, jogadorVez + 1);
+		// vencedor = verificaEmpate(contaPecaTotal, tamanhoMatriz, matriz, jogadorVez + 1);
+		// vencedor = verificaVencedorVertical(tamanhoMatriz, matriz, jogadorVez + 1);
 		// define proximo jogador
+		vencedor = verificaVencedor(tamanhoMatriz, matriz, jogadorVez + 1);
 		jogadorVez = alteraJogador(jogadorVez);
-		
-		
+
+		contaPecaTotal = contaPecaTotal - 1;
+
 	}
-	
-	// como o jogador da vez foi alterado dentro do while, alteramos de novo para pegar o vencedor
-	int jogadorVencedor = alteraJogador(jogadorVez);
-	
-	printf("\nTemos um vencedor! \n");
-	
-	escreveMatriz(tamanhoMatriz, matriz);
-	
-	printf("\nJogador %d venceu! \n", jogadorVencedor + 1);
-	
+	if(verificaEmpate == 1){
+	    printf("\nTemos um Empate! \n");
+	    printf("Parou");
+	} else{
+    	// como o jogador da vez foi alterado dentro do while, alteramos de novo para pegar o vencedor
+    	int jogadorVencedor = alteraJogador(jogadorVez);
+
+    	printf("\nTemos um vencedor! \n");
+
+    	escreveMatriz(tamanhoMatriz, matriz);
+
+    	printf("\nJogador %d venceu! \n", jogadorVencedor + 1);
+    	printf("Parou");
+    }
 	return 0;
 }
-
-
